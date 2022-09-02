4b78839df73008f09b7a2e4d7d1e519ec4a3e450
gunturarman <112367611+gunturarman@users.noreply.github.com>
2022-09-01 20:19:43 -0700

    Add files via upload

---
+++ python5 tkj3.py
@@ -0,0 +1,28 @@
+sisi1=10
+sisi2=10
+luas_p=sisi1*sisi2
+
+print("luas persegi adalah",luas_p)
+
+
+alas=20
+tinggi=10
+luas_S= alas * tinggi/2
+
+print("luas segitiga adalah",luas_S)
+
+
+
+alas=15
+tinggi=15
+luas_J=alas*tinggi/2
+
+
+print("luas jajargenjang adalah",luas_J)
+
+
+'''programan dinamis'''
+
+print("\n1.luas persegi")
+luas_p= sisi*sisi
+print("luas persegi adalah",luas_p)
