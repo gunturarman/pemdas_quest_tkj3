167369a6ffedf1a6f2b66da9cfc423623b28b3ec
gunturarman <112367611+gunturarman@users.noreply.github.com>
2022-09-04 21:57:00 -0700

    Add files via upload

---
+++ python6-tkj3.py
@@ -0,0 +1,13 @@
+dolar=int(input("masukan nilai dolar:" ))
+nilI_r= dolar * 14973,8
+print
+
+
+euro=int(input("masukan nilai euro:" ))
+nilai_w= euro * 1359,22
+print
+
+
+yen=int(input("masukan nilai yen:" ))
+nilai_r= yen * 106,25
+print
