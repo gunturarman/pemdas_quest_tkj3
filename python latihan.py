2e891ef7d510b26a86a4e4f16531c6d741422201
gunturarman <112367611+gunturarman@users.noreply.github.com>
2022-09-07 23:20:23 -0700

    Add files via upload

---
+++ python latihan.py
@@ -0,0 +1,45 @@
+#PERSGI
+
+sisi=int(input("masukan nilai sisi"))
+luas_p=sisi*sisi
+
+print("luas persegi adalah",luas_p)
+
+#segitiga
+
+alas=int(input("masukan nilai alas"))
+tinggi=int(input("masukan nilai tinggi"))
+luas_S= alas * tinggi/2
+
+print("luas segitiga adalah",luas_S)
+
+#jajargenjang
+
+alas=int(input("masukan nilai alas"))
+tinggi=int(input("masukan nilai tinggi"))
+luas_J=alas*tinggi/2
+
+
+print("luas jajargenjang adalah",luas_J)
+
+
+#persegi panjang
+
+panjang=int(input("masukan nilai panjang"))
+lebar=int(input("masukan nilai lebar"))
+luas_P=panjang * lebar
+
+
+print("luas persegi panjang adalah",luas_P)
+
+
+#trapesium
+
+panjang=int(input("masukan nilai panjang"))
+tinggi=int(input("masukan nilai tinggi"))
+luas_T=panjang * tinggi
+
+print("luas terapesium adalah",luas_T)
+
+
+
