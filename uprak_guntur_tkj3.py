5d090fcbca29bddf7fe92d3cdd0b694b7da5787f
gunturarman <112367611+gunturarman@users.noreply.github.com>
2022-09-08 19:41:33 -0700

    Add files via upload

---
+++ uprak_guntur_tkj3.py
@@ -0,0 +1,43 @@
+#balok
+
+panjang=10
+lebar=10
+tinggi=10
+luas_B=panjang*lebar*tinggi
+
+print("luas balok adalah",luas_B)
+
+#kubus
+
+sisi1=15
+sisi2=15
+sisi3=10
+luas_K=sisi1*sisi2*sisi3
+
+print("luas kubus adalah",luas_K)
+
+#limas
+
+luas=10
+tinggi=10
+luas_L= 1/3*luas*tinggi
+
+print("luas limas adalah",luas_L)
+
+
+#tabung
+
+ruang1=10
+ruang2=10
+tinggi=10
+luas_T=22/7*ruang1*ruang2*tinggi
+
+print("luas tabung adalah",luas_T)
+
+
+#sintak
+
+celcius=int(input("masukan nilai celcius"))
+nilai_R=4/5
+print
+                  
