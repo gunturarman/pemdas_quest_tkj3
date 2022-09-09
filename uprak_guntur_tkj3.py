c036dc7da5e040d88c7374ad0874740f535a0823
gunturarman <112367611+gunturarman@users.noreply.github.com>
2022-09-08 19:58:07 -0700

    Add files via upload

--- uprak_guntur_tkj3.py
+++ uprak_guntur_tkj3.py
@@ -1,43 +1,39 @@
-#balok
+# Rumus Volume Balok
+print ("\n1.Volume Balok")
+panjang = int(input("masukkan panjang :"))
+lebar= int(input("masukkan lebar: "))
+tinggi= int(input("masukkan tinggi: "))
+volume_balok = panjang * lebar * tinggi
 
-panjang=10
-lebar=10
-tinggi=10
-luas_B=panjang*lebar*tinggi
+print ("Volume Balok Adalah",volume_balok)
 
-print("luas balok adalah",luas_B)
+# Rumus Volume Kubus
+print ("\n2.Volume Kubus")
+sisi = int(input("masukkan nilai sisi: "))
+volume_kubus = sisi * sisi * sisi
 
-#kubus
+print ("Volume kubus adalah", volume_kubus)
 
-sisi1=15
-sisi2=15
-sisi3=10
-luas_K=sisi1*sisi2*sisi3
+# Rumus Volume Limas
+print ("\n3.Volume Limas")
+sisi_l = int(input("masukkan nilai sisi_l: "))
+tinggi_l = int(input("masukkan nilai tinggi_l: "))
+luas_l = sisi_l * sisi_l
+volume_limas = luas_l * tinggi_l
 
-print("luas kubus adalah",luas_K)
+print ("volume limas adalah",volume_limas)
 
-#limas
+# Rumus volume tabung
+print ("\n4. Volume Tabung")
+r = int(input("masukkan nilai r: "))
+tinggi_t = int(input("masukkan nilai tinggi_t: "))
+volume_tabung = 22/7 * r * r * tinggi_t
 
-luas=10
-tinggi=10
-luas_L= 1/3*luas*tinggi
+print ("volume tabung adalah",volume_tabung)
 
-print("luas limas adalah",luas_L)
+# Celcius Ke Reamur
+print ("\n5.Celcius ke Reamur")
+C = int(input("masukkan nilai Celcius: "))
+Reamur = 4/5 * C
 
-
-#tabung
-
-ruang1=10
-ruang2=10
-tinggi=10
-luas_T=22/7*ruang1*ruang2*tinggi
-
-print("luas tabung adalah",luas_T)
-
-
-#sintak
-
-celcius=int(input("masukan nilai celcius"))
-nilai_R=4/5
-print
-                  
+print ("Celcius ke Reamur adalah",Reamur)
